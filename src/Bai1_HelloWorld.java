d34adb9d05837f2eab63f7d02fe3e7587ddbdbaa
tngan123 <ntuyetngan78@gmail.com>
2024-09-10 08:35:16 +0700

    Hello thế giới mới

---
+++ src/Bai1_HelloWorld.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Administrator
+ */
+public class Bai1_HelloWorld {
+    public static void main(String[] args) {
+            System.out.println("Hello World");
+    }
+}
